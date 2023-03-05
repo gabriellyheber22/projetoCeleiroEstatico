ab90634231fc894204e86fc157e149a32da77c25
gabriellyheber22 <124007358+gabriellyheber22@users.noreply.github.com>
2023-03-05 07:20:32 -0300

    Atualização

--- javascript/responsividade.js
+++
@@ -1,7 +0,0 @@
-function mudouTamanho() {
-    if (window.innerWidth >= 768) {/*Se o tamanho da minha tela for maior ou igual a 768px*/
-        itens.style.display = 'block'
-    } else {
-        itens.style.display = 'none'
-    }
-}
